1287483dfc7fccb30f2a89c04c1e3b05ef151650
Krzysztof Druciarek <kkd26@cam.ac.uk>
2021-11-19 03:15:00 +0000

    Add types, context, substitution and unification algorithm

---
+++ lib/type.ml
@@ -0,0 +1,58 @@
+type typeVar = int
+
+type monoType =
+  | Var of typeVar
+  | Int
+  | Bool
+  | Unit
+  | Pair of monoType * monoType
+  | Fun of monoType * monoType
+
+type typeCtx = (typeVar * monoType) list
+
+let empty_ctx : typeCtx = []
+let update_ctx (ctx : typeCtx) x t : typeCtx = (x, t) :: ctx
+let find (x : typeVar) (ctx : typeCtx) : monoType = snd (List.nth ctx x)
+
+type substitution = (typeVar * monoType) list
+
+let empty_subst : substitution = []
+
+let rec subst (s : monoType) (x : typeVar) (t : monoType) : monoType =
+  match t with
+  | Var y -> if x = y then s else t
+  | Fun (t1, t2) -> Fun (subst s x t1, subst s x t2)
+  | Pair (t1, t2) -> Pair (subst s x t1, subst s x t2)
+  | t -> t
+
+let apply_subst (s : substitution) (t : monoType) : monoType =
+  List.fold_right (fun (x, s) -> subst s x) s t
+
+let applySubstToCtx s (ctx : typeCtx) : typeCtx =
+  List.map (fun (x, t) -> (x, apply_subst s t)) ctx
+
+let comb s1 s2 : substitution =
+  List.map (fun (x, t) -> (x, apply_subst s1 t)) (s1 @ s2)
+
+let subst_from_list list : substitution =
+  let n = List.length list in
+  let range = List.init n (fun x -> x) in
+  List.combine range list
+
+exception TypeException
+exception UnifyException of string
+
+let rec typeExprToString = function
+  | Var n -> String.make 1 (Char.chr (Char.code 'a' + n))
+  | Int -> "int"
+  | Bool -> "bool"
+  | Unit -> "unit"
+  | Pair (t1, t2) ->
+      "(" ^ typeExprToString t1 ^ ", " ^ typeExprToString t2 ^ ")"
+  | Fun (t1, t2) ->
+      "(" ^ typeExprToString t1 ^ ")" ^ " -> " ^ typeExprToString t2
+
+let substToString =
+  let elemToString a (x, t) =
+    a ^ string_of_int x ^ ":(" ^ typeExprToString t ^ ") " in
+  List.fold_left elemToString ""
---
+++ lib/unify.ml
@@ -0,0 +1,37 @@
+open Type
+
+let rec inType n t =
+  match t with
+  | Var m ->
+      if n = m then raise (UnifyException "Circular dependencies ") else true
+  | Pair (t1, t2) -> inType n t1 && inType n t2
+  | Fun (t1, t2) -> inType n t1 && inType n t2
+  | _ -> true
+
+let rec unify_one (t1 : monoType) (t2 : monoType) : substitution =
+  match (t1, t2) with
+  | Int, Int | Bool, Bool | Unit, Unit -> empty_subst
+  | Var n, Var m -> if n = m then empty_subst else [(n, t2)]
+  | Fun (t3, t4), Fun (t5, t6) -> unify [(t3, t5); (t4, t6)]
+  | Pair (t3, t4), Pair (t5, t6) -> unify [(t3, t5); (t4, t6)]
+  | Var n, t | t, Var n -> (
+    try
+      let _ = inType n t in
+      [(n, t)]
+    with UnifyException e ->
+      raise
+        (UnifyException (e ^ typeExprToString t1 ^ " and " ^ typeExprToString t2)
+        ) )
+  | _, _ ->
+      raise
+        (UnifyException
+           ( "Cannot unify " ^ typeExprToString t1 ^ " and "
+           ^ typeExprToString t2 ) )
+
+and unify s : substitution =
+  match s with
+  | [] -> []
+  | (x, y) :: t ->
+      let t2 = unify t in
+      let t1 = unify_one (apply_subst t2 x) (apply_subst t2 y) in
+      t1 @ t2
