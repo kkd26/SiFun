e23811f52f2b37940af4a5c09037fffc4bf7da74
Krzysztof Druciarek <kkd26@cam.ac.uk>
2021-11-22 00:33:33 +0000

    Fix type inference in pair, fst and snd

--- lib/infer.ml
+++ lib/infer.ml
@@ -29,30 +29,31 @@ let inferType (e : Debruijn.expr) : substitution * monoType =
         let tx = Fun (tg, Var x) in
         try
           let s3 = unify [(applySubstToMonoType s2 tf, tx)] in
-          return (combineSubst s3 (combineSubst s2 s1), applySubstToMonoType s3 (Var x))
+          return
+            ( combineSubst s3 (combineSubst s2 s1)
+            , applySubstToMonoType s3 (Var x) )
         with UnifyException e -> failwith (e ^ substToString ctx) )
     | Pair (e1, e2) ->
         inferType' ctx e1
         >>= fun (s1, t1) ->
-        inferType' ctx e2 >>= fun (s2, t2) -> return (combineSubst s1 s2, Pair (t1, t2))
-    | Fst e ->
+        let newCtx = applySubstToCtx s1 ctx in
+        inferType' newCtx e2
+        >>= fun (s2, t2) ->
+        let s3 = combineSubst s2 s1 in
+        return
+          (s3, Pair (applySubstToMonoType s3 t1, applySubstToMonoType s3 t2))
+    | (Fst e | Snd e) as e1 ->
         inferType' ctx e
-        >>= fun (_, t1) ->
+        >>= fun (s1, t1) ->
         freshName
         >>= fun x ->
         freshName
         >>= fun y ->
         let tx = Pair (Var x, Var y) in
         let s2 = unify [(tx, t1)] in
-        return (s2, applySubstToMonoType s2 (Var x))
-    | Snd e ->
-        inferType' ctx e
-        >>= fun (_, t1) ->
-        freshName
-        >>= fun x ->
-        freshName
-        >>= fun y ->
-        let tx = Pair (Var x, Var y) in
-        let s2 = unify [(tx, t1)] in
-        return (s2, applySubstToMonoType s2 (Var y)) in
+        let s3 = combineSubst s2 s1 in
+        return
+          ( s3
+          , applySubstToMonoType s3 (match e1 with Fst _ -> Var x | _ -> Var y)
+          ) in
   snd (runState (inferType' emptyCtx e) ~init:0)
