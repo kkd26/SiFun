7ab67a5074312c3c345d7119408152558cd0a2d7
Krzysztof Druciarek <kkd26@cam.ac.uk>
2021-11-23 15:56:50 +0000

    Fix pretty printing for type expresions

--- lib/type.ml
+++ lib/type.ml
@@ -42,15 +42,22 @@ let substFromList list : substitution =
 exception TypeException
 exception UnifyException of string
 
+let numToString n =
+  let i = n / 26 in
+  String.make 1 (Char.chr (Char.code 'a' + (n mod 26)))
+  ^ if i <> 0 then string_of_int i else ""
+
 let rec typeExprToString = function
-  | Var n -> String.make 1 (Char.chr (Char.code 'a' + n))
+  | Var n -> numToString n
   | Int -> "int"
   | Bool -> "bool"
   | Unit -> "unit"
   | Pair (t1, t2) ->
       "(" ^ typeExprToString t1 ^ ", " ^ typeExprToString t2 ^ ")"
   | Fun (t1, t2) ->
-      "(" ^ typeExprToString t1 ^ ")" ^ " -> " ^ typeExprToString t2
+      let inner = typeExprToString t1 in
+      (match t1 with Fun (_, _) -> "(" ^ inner ^ ")" | _ -> inner)
+      ^ " -> " ^ typeExprToString t2
 
 let substToString =
   let elemToString a (x, t) =
